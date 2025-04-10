3f207ffd92a0683aa4b2f89a8698d1cc43b4f76d
CHETAN MOREY <chetanmorey881906@gmail.com>
2025-04-10 16:53:55 +0530

    Add files via upload

---
+++ PRACTICAL9/CODE.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set("Asia/Kolkata"); // Set the default timezone
+echo "Current Date: " . date("Y-m-d") . "\n";
+echo "Current Time: " . date("h:i:s A") . "\n";
+echo "Current Day: " . date("l") . "\n";
+?>
+
